b0e1fe4e107c209bdb0072e65f9a77e9591c90cd
신산하 <tksgk2598@naver.com>
2023-09-19 17:54:28 +0900

    Feat: status bar 숨김 처리 & 안내문구 animation 적용

--- frontend/screens/IntroExplain.jsx
+++ frontend/screens/IntroExplain.jsx
@@ -1,13 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import {Text, View} from 'react-native';
-import React from "react";
+import {Text, View, Dimensions} from 'react-native';
+import React, {useState, useEffect} from "react";
 import { Video } from "expo-av";
 import tw from "twrnc";
+import * as Animatable from 'react-native-animatable';
 
 
 const IntroExplain = () => {
+
+    const [animation1, setAnimation1] = useState(null);
+    const [animation2, setAnimation2] = useState(null);
+    const [animation3, setAnimation3] = useState(null);
+
+    const height = Dimensions.get("window").height-150;
+
+    useEffect(() => {
+        if (animation1) animation1.slideInUp(1000); // 첫 번째 애니메이션
+    }, [animation1]);
+
     return(
         <View style={tw`flex-1`}>
+            
+            <StatusBar hidden />
+
             <Video 
                 style={tw`absolute top-0 left-0 right-0 bottom-0`}
                 source={require("../assets/videos/walkingLoad.mp4")}
@@ -16,7 +31,10 @@ const IntroExplain = () => {
                 shouldPlay={true}
                 isLooping={true}
             />
-            <View style={tw`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-30`}></View>
+            <View style={tw`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-40`}></View>
+            <Animatable.View ref={(ref) => setAnimation1(ref)} style={tw`flex-1`}>
+                <Text style={tw`absolute top-0 left-0 right-0 bottom-0 text-white text-5 mt-[${height}] ml-[40%]`}>환영합니다.</Text>
+            </Animatable.View>
         </View>
     )
 }
