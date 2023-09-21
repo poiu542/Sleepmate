35360fc70d90f14240d1acd2536ac90189cb616a
Daen12 <sdy971213@gmail.com>
2023-09-21 17:27:40 +0900

    feat: webview 설치

--- frontend/screens/Test.jsx
+++ frontend/screens/Test.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import WebView from 'react-native-webview';
 
 function Test() {
+    //우리의 배포된 웹사이트
     return (
-        <View>
-
-            
-        </View>
+        <WebView 
+            source={{uri : "https://www.google.co.kr/" }}>
+        </WebView>
     );
 }
 
