b7b0d6019fb65815f445700e2fb2f98d3749c3b2
WonjunChun <chunjh1103@naver.com>
2023-09-24 15:26:25 +0900

    Feat/BE : WatchController의 getLuxAndSleepTime 메서드 작성
    - /watch/luxAndTime : 일어나기 버튼 클릭 시, 30분 전 lux값, 자러간 시간, 일어난 시간 정보를 반환하는 API

---
+++ backend/src/main/java/site/sleepmate/backend/controller/WatchController.java
@@ -0,0 +1,29 @@
+package site.sleepmate.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.sleepmate.backend.dto.WakeUpResponseDto;
+import site.sleepmate.backend.service.WatchService;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RequestMapping("/watch")
+@RestController
+public class WatchController {
+    private final WatchService watchService;
+
+    @GetMapping("/luxAndTime")
+    public ResponseEntity<WakeUpResponseDto> getLuxAndSleepTime(){
+//        LocalDateTime nowTime = LocalDateTime.now();
+        LocalDateTime nowTime = watchService.getLastRecord().getTime();
+        WakeUpResponseDto wakeUpResponseDto = watchService.getLuxAndSleepTime(nowTime);
+        return new ResponseEntity<>(wakeUpResponseDto, HttpStatus.OK);
+    }
+
+
+}
