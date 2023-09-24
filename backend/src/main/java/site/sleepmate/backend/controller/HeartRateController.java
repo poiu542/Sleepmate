d7fcb9d729c8567df514815d35717dd76a64cbe7
bluest12 <bluest12@naver.com>
2023-09-25 01:23:07 +0900

    Feat/BE : HeartRateController 생성

---
+++ backend/src/main/java/site/sleepmate/backend/controller/HeartRateController.java
@@ -0,0 +1,32 @@
+package site.sleepmate.backend.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.sleepmate.backend.service.AbnormalHeartRateMeasurementService;
+import site.sleepmate.backend.service.NormalHeartRateMeasurementService;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/heartRate")
+public class HeartRateController {
+
+    private final AbnormalHeartRateMeasurementService abnormalHeartRateMeasurementService;
+    private final NormalHeartRateMeasurementService normalHeartRateMeasurementService;
+
+    @GetMapping("/abnormal")
+    public ResponseEntity<String> abnormalMeasurement(){
+        abnormalHeartRateMeasurementService.getAbnormalSituation();
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
+    @GetMapping("/normal")
+    public ResponseEntity<String> normalMeasurement() {
+        normalHeartRateMeasurementService.getAveBPM();
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
+}
