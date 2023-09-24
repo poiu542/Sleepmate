8a6f301557f6eed8c86b9792cbb05938ec941af4
WonjunChun <chunjh1103@naver.com>
2023-09-24 15:23:50 +0900

    Feat/BE : WatchService 작성
    - getLuxAndSleepTime 메서드 작성 : 기상 30분 전의 lux값과 자러간 시간, 일어난 시간 반환하는 메서드(WakeUpResponseDto)
    - getLastRecord 메서드 작성 : 가장 최근 VideoRecord를 가져오기 위한 메서드

---
+++ backend/src/main/java/site/sleepmate/backend/service/WatchService.java
@@ -0,0 +1,50 @@
+package site.sleepmate.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.sleepmate.backend.domain.LuxRecord;
+import site.sleepmate.backend.domain.VideoRecord;
+import site.sleepmate.backend.dto.WakeUpResponseDto;
+import site.sleepmate.backend.repository.LuxRecordRepository;
+import site.sleepmate.backend.repository.VideoOrderRepository;
+import site.sleepmate.backend.repository.VideoRecordRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class WatchService {
+    private final LuxRecordRepository luxRecordRepository;
+    private final VideoOrderRepository videoOrderRepository;
+    private final VideoRecordRepository videoRecordRepository;
+
+    //30분 전의 lux값과 자러간 시간, 일어난 시간 반환.
+    public WakeUpResponseDto getLuxAndSleepTime(LocalDateTime time){
+        //lux값 조회
+        LocalDateTime before30MinTime = time.minusMinutes(30); //30분 전 시각
+        LocalDateTime startTime = LocalDateTime.of(before30MinTime.getYear(), before30MinTime.getMonthValue(),
+                before30MinTime.getDayOfMonth(), before30MinTime.getHour(), before30MinTime.getMinute(), 0);
+        LocalDateTime endTime = startTime.plusMinutes(1);
+        List<LuxRecord> luxs = luxRecordRepository.findByTime(startTime, endTime);
+        LuxRecord lux = luxs.get(0);
+
+        //자러간 시간, 일어난 시간 조회
+        LocalDate date = getLastRecord().getSleepDate(); //가장 최근 취침 날짜 가져옴
+        Optional<VideoRecord> startSleep = videoRecordRepository.findTop1BySleepDateOrderByVideoSeqAsc(date);
+        Optional<VideoRecord> endSleep = videoRecordRepository.findTop1BySleepDateOrderByVideoSeqDesc(date);
+
+        WakeUpResponseDto wakeUpResponseDto = WakeUpResponseDto.fromEntities(lux, startSleep.get(), endSleep.get());
+        return wakeUpResponseDto;
+    }
+
+    public VideoRecord getLastRecord(){
+        return videoRecordRepository.findTop1ByOrderByVideoSeqDesc().get();
+    }
+
+
+}
