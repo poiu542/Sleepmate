dfd8d18aa385b97fb265078a50ecaff411a68aea
WonjunChun <chunjh1103@naver.com>
2023-09-25 10:31:03 +0900

    Chore/BE : 불필요한 줄바꿈 제거
    - 불필요한 줄바꿈 제거

--- backend/src/main/java/site/sleepmate/backend/repository/VideoRecordRepository.java
+++ backend/src/main/java/site/sleepmate/backend/repository/VideoRecordRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 public interface VideoRecordRepository extends JpaRepository<VideoRecord, Long> {
     //취침 시작 시간의 로그 가져오는 쿼리
     Optional<VideoRecord> findTop1BySleepDateOrderByVideoSeqAsc(LocalDate date);
-
     //취침 종료 시간의 로그 가져오는 쿼리
     Optional<VideoRecord> findTop1BySleepDateOrderByVideoSeqDesc(LocalDate date);
     Optional<VideoRecord> findTop1ByOrderByVideoSeqDesc();
