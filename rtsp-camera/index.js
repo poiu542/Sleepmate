f23dbc1abea3bccac0b9a1d4015153f48fdc4875
leverest96 <myjade0416@gmail.com>
2023-09-25 13:51:38 +0900

    Refactor/FE : rtsp streamUrl 수정

--- rtsp-camera/index.js
+++ rtsp-camera/index.js
@@ -1,10 +1,10 @@
-Stream = require('node-rtsp-stream')
+Stream = require("node-rtsp-stream");
 stream = new Stream({
-	name: 'name',
-	streamUrl: 'rtsp://admin:ssafy@192.168.119.162:554/stream_ch00_0',
-	wsPort: 9999,
-	ffmpegOptions: { 
-		'-stats': '', 
-		'-r': 30 
-	}
-})
+  name: "name",
+  streamUrl: "rtsp://admin:ssafy@sleepmate5.iptimecam.com:21324/stream_ch00_0",
+  wsPort: 9999,
+  ffmpegOptions: {
+    "-stats": "",
+    "-r": 30,
+  },
+});
