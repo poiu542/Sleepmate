5500807606c8e57cc806097fffd649f7320e79c8
Daen12 <sdy971213@gmail.com>
2023-09-26 16:53:50 +0900

    Feat: 모달 생성

--- frontend/components/ReportBlock/Circadian.jsx
+++ frontend/components/ReportBlock/Circadian.jsx
@@ -1,47 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View, ImageBackground, Image, ScrollView, Pressable } from 'react-native';
 import sun from "../../assets/images/Sunny.png"
 import tw from "twrnc"
+import BottomSheetModal from './BottomSheetModal';
 function Circadian(props) {
+    const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false);
+
+    const toggleBottomSheet = () => {
+        setIsBottomSheetVisible(!isBottomSheetVisible);
+    };
     return (
         <>
-        <View style={tw `flex-1 w-full bg-[#fff]/70 rounded-2 p-3 mb-5`}>
+        <View style={tw `flex-1 w-full bg-[#fff]/70 rounded-5 p-3 mb-4`}>
                     {/* header */}
                     <View style={tw `flex-1 flex-row`}>
-                        <View style={tw `flex-2 border-2`}>
-                            <Text style={tw `text-lg`}>나의 일주기 리듬</Text>
+                        <View style={tw `flex-2 `}>
+                            <Text style={tw `text-lg text-[#333] text-[4]`}>나의 일주기 리듬</Text>
                         </View>
-                        <Pressable style={tw `flex-1 border-2 justify-center`} onPress={()=>{console.log("hi")}}>
+                        <Pressable style={tw `flex-1  justify-center`} onPress={toggleBottomSheet}>
                             <Text style={tw `text-[#555] text-xs`}>&#62; more</Text>
                         </Pressable>
-                        <View style={tw `flex-1 border-2 items-end`}>
+                        <View style={tw `flex-1  items-end`}>
                             <View style={tw `rounded-1.5 w-15 p-1.5 bg-[#251751]/50 items-center justify-center`}><Text style={tw `text-white font-bold`}>정상</Text></View>
                         </View>
                     </View>
                     {/* body */}
                     <View style={tw `flex-2 flex-row p-2`}>
-                        <View style={tw `flex-2 border-2 items-center p-2`}>
-                            <Image source={sun} style={tw `h-8 mb-1`}></Image>
-                            <Text style={tw `text-[#333] text-xs font-bold -mb-1`}>기상 시 조도</Text>
+                        <View style={tw `flex-2  items-center p-0`}>
+                            <Image source={sun} style={tw `h-11 mb-1`}></Image>
+                            <Text style={tw `text-[#333] text-[2.5] font-bold -mb-1`}>기상 시 조도</Text>
                             <Text style={tw `text-[#222] text-base font-bold`}>50 Lux</Text>
                         </View>
-                        <View style={tw `flex-1 border-2 justify-center`} onPress={()=>{console.log("hi")}}>
+                        <View style={tw `flex-1  justify-center mt-4`} onPress={()=>{console.log("hi")}}>
                             <Text style={tw `text-[#333] text-xs font-bold mb-4`}>잠에 든 시간</Text>
                             <Text style={tw `text-[#333] text-xs font-bold`}>깨어난 시간</Text>
                         </View>
-                        <View style={tw `flex-1 border-2 items-start justify-center`}>
-                            <Text style={tw `border-2 text-[#333] text-base font-bold mb-1`}>23:50</Text>
-                            <Text style={tw `border-2 text-[#333] text-base font-bold`}>07:45</Text>
+                        <View style={tw `flex-1  items-start justify-center mt-4`}>
+                            <Text style={tw ` text-[#333] text-base font-bold mb-2`}>23:50</Text>
+                            <Text style={tw ` text-[#333] text-base font-bold`}>07:45</Text>
                         </View>
                     </View>
                     {/* tail */}
                     <View style={tw `flex-1 items-center`}>
-                        <View style={tw `rounded-2 border-[#fff] border-[0.25] p-1 items-center `}>
-                            <Text style={tw `text-[3.2] text-[#222] tracking-tight`}>일주기 리듬에 적절한 수면을 하고 계십니다.</Text>
-                            <Text style={tw `text-[3.2] text-[#222] tracking-tight`}>기상 시 밝은 환경으로 건강한 생체리듬에 기여하고 계시네요!</Text>
+                        <View style={tw `rounded-2 border-[#ddd] border-[0.25] p-1 items-center w-full`}>
+                            <Text style={tw `text-[3.2] text-[#333] tracking-tight font-bold leading-5`}>일주기 리듬에 적절한 수면을 하고 계십니다.</Text>
+                            <Text style={tw `text-[3.2] text-[#333] tracking-tight font-bold`}>기상 시 밝은 환경으로 건강한 생체리듬에 기여하고 계시네요!</Text>
                         </View>
                     </View>
                 </View>
+                <BottomSheetModal isVisible={isBottomSheetVisible} onClose={toggleBottomSheet} modalN={1} />
         </>
     );
 }
--- frontend/screens/Diagnosis.jsx
+++ frontend/screens/Diagnosis.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import { Text, View, ImageBackground, Image, ScrollView, Pressable } from 'react-native';
+import { Text, View, ImageBackground, Image, ScrollView, Pressable, Modal } from 'react-native';
 import moon from "../assets/images/Diagnosis-background.png"
 import title from "../assets/images/Diagnosis-title.png"
 import sun from "../assets/images/Sunny.png"
 import tw from "twrnc"
 import Circadian from '../components/ReportBlock/Circadian';
 import Rem from '../components/ReportBlock/Rem';
+import Breath from '../components/ReportBlock/Breath';
 function Diagnosis() {
     return (
         <>
         <View style={tw `flex flex-1`}>
             <ImageBackground source={moon} style={tw `w-full h-full absolute`}></ImageBackground>
-            <Image style={tw `mt-15 ml-5`} source={title}></Image>
+            <Image style={tw `mt-15 ml-5 mb-1`} source={title}></Image>
             <ScrollView style={tw `p-3`}>
                 {/* 진단박스1 */}
                 <Circadian></Circadian>
+                <Breath></Breath>
                 <Rem></Rem>
             </ScrollView>
+            {/* <Modal
+                animated
+                animationType="fade"
+                visible={true}
+                transparent
+                onRequestClose={() => this._handleDismiss()}>
+                <View style={tw `bg-[#fff]/90 flex-1 justify-end`}>
+                    
+                </View>
+            </Modal> */}
         </View>
         </>
     );
