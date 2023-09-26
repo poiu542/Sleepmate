b2a4045c388699076e2cc12689873bde67a05b0a
신산하 <tksgk2598@naver.com>
2023-09-27 02:00:52 +0900

    Feat: 음성 인식 기능 시도중

--- frontend/screens/Sleep.jsx
+++ frontend/screens/Sleep.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av';
+import * as Speech from 'expo-speech';
+
 function Sleep() {
+
+    const [text, setText] = useState('');
+    const [speaking, setSpeaking] = useState(false);
+
+    // 음성 인식 함수
+    const startRecognition = async () => {
+        try {
+        const { status } = await Speech.requestPermissionsAsync();
+        if (status === 'granted') {
+            setSpeaking(true);
+            const result = await Speech.recognizeAsync();
+            if (result !== undefined) {
+            setText(result.text);
+            }
+            setSpeaking(false);
+        } else {
+            console.error('음성 권한이 거부되었습니다.');
+        }
+        } catch (error) {
+        console.error('음성 인식 중 오류 발생:', error);
+        }
+    };
+
     return (
-        <>
-        <Text>Sleep Page 1</Text>
-        </>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity
+            onPress={startRecognition}
+            disabled={speaking}
+            style={{
+            backgroundColor: speaking ? 'gray' : 'blue',
+            padding: 10,
+            borderRadius: 5,
+            }}
+        >
+            <Text style={{ color: 'white' }}>
+            {speaking ? '음성 인식 중...' : '음성 인식 시작'}
+            </Text>
+        </TouchableOpacity>
+        <Text style={{ marginTop: 20 }}>{text}</Text>
+        </View>
     );
 }
 
