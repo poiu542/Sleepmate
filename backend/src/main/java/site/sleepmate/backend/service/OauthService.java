addd124326a27d675dd9ef578b4b4cd143add0d4
bluest12 <bluest12@naver.com>
2023-09-26 15:15:06 +0900

    Refact/BE : KakaoLoginRequestDto 클래스 수정

--- backend/src/main/java/site/sleepmate/backend/service/OauthService.java
+++ backend/src/main/java/site/sleepmate/backend/service/OauthService.java
@@ -135,10 +135,9 @@ public class OauthService {
                 .alarm(Time.valueOf("07:00:00"))
                 .noServey(true)
                 .crescendo(false)
+                .weight(0)
+                .height(0)
                 .build());
-
         br.close();
-
-
     }
 }
