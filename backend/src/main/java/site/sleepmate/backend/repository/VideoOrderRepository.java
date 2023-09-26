ff1f71e357640a8fa2e6a356b79782e7ab9c4812
bluest12 <bluest12@naver.com>
2023-09-27 00:05:15 +0900

    Refact/BE : VideoOrderRepository, AbnormalHeartRateMeasurmentService 수정
    VideoOrderRepository -> member도 검색 조건에 포함
    AbnormalHeartRateMeasurementService -> 로직에 주석 처리

--- backend/src/main/java/site/sleepmate/backend/repository/VideoOrderRepository.java
+++ backend/src/main/java/site/sleepmate/backend/repository/VideoOrderRepository.java
@@ -1,11 +1,12 @@
 package site.sleepmate.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.sleepmate.backend.domain.Member;
 import site.sleepmate.backend.domain.VideoOrder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface VideoOrderRepository extends JpaRepository<VideoOrder, Integer> {
-    List<VideoOrder> findAllBySleepDateOrderByStartTime(LocalDate sleepDate);
+    List<VideoOrder> findAllByMemberAndSleepDateOrderByStartTime(Member member, LocalDate sleepDate);
 }
--- backend/src/main/java/site/sleepmate/backend/service/AbnormalHeartRateMeasurementService.java
+++ backend/src/main/java/site/sleepmate/backend/service/AbnormalHeartRateMeasurementService.java
@@ -24,22 +24,28 @@ public class AbnormalHeartRateMeasurementService {
 
     // 감지된 시간 & 이상 심박수 & 해당 자세 반환 메서드
     public List<AbnormalResponseDto> getAbnormalSituation(Member member, LocalDate sleepDate) {
-        // 수면 날짜의 데이터 가져오기(수정해야함)
+        // 특정 회원의 심박수 기록 가져오기
         List<HeartRateRecord> heartRateRecords = heartRateRecordRepository.findAllByMemberAndSleepDateOrderByTime(member, sleepDate);
-        List<VideoOrder> videoOrders = videoOrderRepository.findAllBySleepDateOrderByStartTime(sleepDate);
+        // 특정 회원의 영상 기록 가져오기
+        List<VideoOrder> videoOrders = videoOrderRepository.findAllByMemberAndSleepDateOrderByStartTime(member, sleepDate);
 
+        // 최종 반환할 List(감지 시간, 감지된 심박수, 해당 자세)
         List<AbnormalResponseDto> abnormalResponseDtos = new ArrayList<>();
+        // 로직 중간에 필요한 List(감지 시간, 감지된 심박수)
         List<AbnormalPartDto> abnormalPartDtos = new ArrayList<>();
 
+        // 세 가지 정보(감지 시간, 감지된 심박수, 해당 자세)가 담긴 DTO
         AbnormalResponseDto abnormalResponseDto = new AbnormalResponseDto();
+        // 두 가지 정보(감지 시간, 감지된 심박수)가 담긴 DTO
         AbnormalPartDto abnormalPartDto = new AbnormalPartDto();
-
+        // 감지된 시간 List
         List<LocalDateTime> detectedTimes = new ArrayList<>();
 
         // 이상 시간을 담은 Set
         String detectedTime = "";
         int dectedHeartrate = 0;
 
+        // DateTime -> String 변경
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
         // 이상 심박수일때, detectedTimeAndHeartRate에 시간, 이상 심박수 담기
@@ -58,6 +64,7 @@ public class AbnormalHeartRateMeasurementService {
 
         for (VideoOrder videoOrder : videoOrders) {
             for (int i = detectedTimes.size(); i > 0; i--){
+                // 감지된 시간이 시작시간보다 나중이고, 종료 시간보다 이전일때 데이터 입력
                 if (videoOrder.getStartTime().isBefore(detectedTimes.get(i)) && videoOrder.getEndTime().isAfter(detectedTimes.get(i))) {
                     abnormalResponseDtos.add(AbnormalResponseDto.getAbnormalData(abnormalPartDto.getDetectedTime(), abnormalPartDto.getAbnormalHeartRate(), videoOrder.getPosture()));
                 }
