90efb28a343a1058656d2487cbdfca43755b41f5
bluest12 <bluest12@naver.com>
2023-10-02 22:07:13 +0900

    Refactor/BE : HeartRateRequestDto클래스 생성

--- backend/src/main/java/site/sleepmate/backend/controller/HeartRateController.java
+++ backend/src/main/java/site/sleepmate/backend/controller/HeartRateController.java
@@ -3,12 +3,10 @@ package site.sleepmate.backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.sleepmate.backend.domain.Member;
 import site.sleepmate.backend.dto.AbnormalResponseDto;
+import site.sleepmate.backend.dto.HeartRateRequestDto;
 import site.sleepmate.backend.dto.NormalResponseDto;
 import site.sleepmate.backend.service.AbnormalHeartRateMeasurementService;
 import site.sleepmate.backend.service.HeartRateJudgementService;
@@ -19,20 +17,20 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/heartRate")
+@RequestMapping("/api/health")
 public class HeartRateController {
     private final AbnormalHeartRateMeasurementService abnormalHeartRateMeasurementService;
     private final NormalHeartRateMeasurementService normalHeartRateMeasurementService;
     private final HeartRateJudgementService heartRateJudgementService;
 
-    @GetMapping("/{memberSeq}/{sleepDate}")
-    public  ResponseEntity<?> getHeartRate(@PathVariable Long memberSeq, @PathVariable LocalDate sleepDate) {
-        Integer measurement = heartRateJudgementService.getJudgement(memberSeq, sleepDate);
+    @PostMapping("/heart-rate")
+    public  ResponseEntity<?> getHeartRate(@RequestBody HeartRateRequestDto heartRateRequestDto) {
+        Integer measurement = heartRateJudgementService.getJudgement(heartRateRequestDto.getMemberSeq(), heartRateRequestDto.getSleepDate());
         if (measurement == 0) {
-            NormalResponseDto normalResponseDto = normalHeartRateMeasurementService.getMinAndMaxBPM(memberSeq, sleepDate);
+            NormalResponseDto normalResponseDto = normalHeartRateMeasurementService.getMinAndMaxBPM(heartRateRequestDto.getMemberSeq(), heartRateRequestDto.getSleepDate());
             return new ResponseEntity<>(normalResponseDto, HttpStatus.OK);
         } else {
-            List<AbnormalResponseDto> abnormalResponseDtos = abnormalHeartRateMeasurementService.getAbnormalSituation(memberSeq, sleepDate);
+            List<AbnormalResponseDto> abnormalResponseDtos = abnormalHeartRateMeasurementService.getAbnormalSituation(heartRateRequestDto.getMemberSeq(), heartRateRequestDto.getSleepDate());
             return new ResponseEntity<>(abnormalResponseDtos, HttpStatus.OK);
         }
     }
