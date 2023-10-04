d216a666c2579371425c84acafb864fe94decf02
신산하 <tksgk2598@naver.com>
2023-10-04 10:37:00 +0900

    Feat: 분석 비디오 수 api 연동

--- frontend/components/Alert/SleepDataArriveAlert.jsx
+++ frontend/components/Alert/SleepDataArriveAlert.jsx
@@ -12,13 +12,39 @@ const SleepDataArriveAlert = ({selectedDate}) => {
 
     const [count, setCount] = useState(0);
 
+
+    //axios 연결
+    const axiosSleepMotionCount = () => {
+        const data = {
+          "memberSeq": 1,
+          "sleepDate": selectedDate
+        }
+        nonAuthHttp.post(`/api/video`, data)
+        .then(response => {
+            const result = response.data;
+            if (result) {
+                setCount(result.result)
+            }
+        })
+        .catch(error => {
+            const err = error;
+            console.log(err);
+        });
+      }
+  
+  
+      useEffect(()=>{
+        axiosSleepMotionCount();
+      },[])
+
+
     return(
 
           <View 
             style={tw`flex-row w-full h-13 bg-[#000]/50 rounded-3 items-center justify-center`}>
               <Image style={tw`w-5 h-5  mr-3`} source={alarm_bell}/>
               <Text style={tw`text-white text-[3.3] text-center font-bold`}>
-                  {count}건의 분석 이미지가 도착했습니다.
+                  {count}건의 분석 비디오가 도착했습니다.
               </Text>
           </View>
     )
